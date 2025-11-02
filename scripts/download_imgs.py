846142233981f9c3a4b959bc731abd7b458e3388
Thanadol Chitthamlerd <thanadolps@gmail.com>
2025-11-03 05:01:58 +0700

    ensure url uniqueness

--- scripts/download_imgs.py
+++ scripts/download_imgs.py
@@ -1,6 +1,8 @@
 from json import load
 from os import system
 from pathlib import Path
+from urllib.parse import quote, unquote
+from re import compile
 
 posts = load(open("data/posts.json"))
 
@@ -13,13 +15,40 @@ if "https://i.ytimg.com/img/no_thumbnail.jpg" in attachment:
 
 
 # Excluded downloaded images
-downloaded = set(d.stem for d in Path("archive_imgs").iterdir() if d.is_file())
+downloaded = set(unquote(d.stem) for d in Path("archive_imgs").iterdir() if d.is_file() and d.stat().st_size > 0)
 to_download = [a for a in attachment if a.split('/')[-1] not in downloaded]
+print(f"to_download={to_download}")
+
 
 # Download the attachments
+names = set()
+pat = compile(r"(.+)=s\d+")
+
 for i, url in enumerate(to_download):
     print(f"Downloading {url} ({i+1}/{len(to_download)})")
-    filename = f"archive_imgs/{url.split('/')[-1]}"
-    system(f"wget -O {filename} {url}")
+    filename = quote(url.replace("https://", "").split('/', 1)[1], safe="")
+    if filename in names:
+        print(f"{filename} already processed")
+        exit(1)
+    names.add(filename)
+    filepath = f"archive_imgs/{filename}"
 
+    status = system(f"wget -O {filepath} {url}")
+    if status != 0:
+        print(f"command return with status {status}")
+    
+    g = pat.match(url)
+    if g is None:
+        exit(1)
+    url = g.group(0)
+    print(f"Failed, trying {url} instead")
+    status = system(f"wget -O {filepath} {url}")
+    if status != 0:
+        print(f"command return with status {status}")
+    url = g.group(1)
+    print(f"Failed, trying {url} instead")
+    status = system(f"wget -O {filepath} {url}")
+    if status != 0:
+        print(f"command return with status {status}")
+        exit(status)
 
